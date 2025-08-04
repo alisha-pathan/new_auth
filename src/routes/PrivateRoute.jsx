36e9ddf6a0695d28ec34378bd4ebd8dbe5e7cb7b
alishapatha <alishapathan401@gmail.com>
2025-08-05 02:34:25 +0530

    Delete src directory

--- src/routes/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Navigate } from 'react-router-dom'
-
-export const PrivateRoute = ({ children }) => {
-  const { token, user } = useSelector(state => state.auth)
-  return token ? children : <Navigate to={'/'} />
-}
